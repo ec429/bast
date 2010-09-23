168f7dce8db26341a1c8aa7cc385b5f629ced4dd
Sound and Fury <ec429@cam.ac.uk>
2010-09-23 11:29:19 +0100

    .gitignore updates

---
+++ version.h
@@ -0,0 +1,13 @@
+/*
+	bast - ZX Basic text to tape
+	
+	Copyright Edward Cree, 2010
+	License: GNU GPL v3+
+	version.h: contains version number (generated from git describe)
+*/
+#pragma once
+#define VERSION_MAJ 0 // Major version
+#define VERSION_MIN 1 // Minor version
+#define VERSION_REV 0 // Revision number
+#define VERSION_TXT "" // Rest of git describe
+#define CC_VERSION	"gcc version 4.3.3 (Ubuntu 4.3.3-5ubuntu4) " // last line of cc -v
